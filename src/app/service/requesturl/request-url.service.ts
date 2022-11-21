bb26f724985a5cbb08fdf9642d3101e63150ff6b
Philipp Posch <philipp.posch@smartsquare.at>
2022-11-21 14:13:57 +0100

    Alter Status ändern kann abgebrochen werden; Layout minimal verändert

--- src/app/service/requesturl/request-url.service.ts
+++ src/app/service/requesturl/request-url.service.ts
@@ -10,7 +10,7 @@ export class RequestUrlService {
    * 0 = test.smartsquare.at
    * 1 = sonar.smartsquare.at
    */
-  public readonly STATE: number = 1;
+  public readonly STATE: number = -1;
 
   //#region Protokollmessage
   public MESSAGE_CREATE: string = "";
